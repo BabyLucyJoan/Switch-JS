9c7fc72a445fc600e5e08771c8febb4842cad685
Lucy Nwokoye <142795917+BabyLucyJoan@users.noreply.github.com>
2023-10-24 16:21:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+let userName = ''
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
+
+let userQuestion = ''
+console.log(`${userName}: ${userQuestion}`)
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '' + randomNumber 
+
+switch(eightBall){
+  case '0' :
+  eightBall = 'It is certain';
+  break;
+   case '1' :
+  eightBall = 'It is decidedly so';
+  break;
+   case '2' :
+  eightBall = 'Reply hazy try again';
+  break;
+   case '3' :
+  eightBall = 'Cannot predict now';
+  break;
+   case '4' :
+  eightBall = 'Do not count on it';
+  break;
+   case '5' :
+  eightBall =  'My sources say no';
+  break;
+  case '6' :
+  eightBall = 'Outlook not so good';
+  break;
+   case '7' :
+  eightBall =  'Signs point to yes';
+  break;
+  default :
+  eightBall = 'Undefined';
+  break;
+}
+console.log(eightBall)
